dbcae55a8a2e3702f1b5b5786d309314979f907f
markoMaksimovic1419S <72449575+markoMaksimovic1419S@users.noreply.github.com>
2020-10-07 12:06:39 +0200

    Add files via upload

---
+++ domaci1/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Domaci 1</title>
+    <style>
+    table, th, td{
+        border: 1px solid black;
+    }
+    </style>
+</head>
+<body>
+    
+    <table>
+    <tr>
+    <th>Ime</th>
+    <th>Prezime</th>
+    <th>Index</th>
+    </tr>
+    <tr>
+    <td>Student 1 ime</td>
+    <td>Student 1 prezime</td>
+    <td>Student 1 index</td>
+    </tr>
+    <tr>
+    <td>Student 2 ime</td>
+    <td>Student 2 prezime</td>
+    <td>Student 2 index</td>
+    </tr>
+    <tr>
+    <td>Student 3 ime</td>
+    <td>Student 3 prezime</td>
+    <td>Student 3 index</td>
+    </tr>
+    <tr>
+    <td>Student 4 ime</td>
+    <td rowspan=2>Student 4 prezime</td>
+    <td>Student 4 index</td>
+    </tr>
+    <tr>
+    <td>Student 5 ime</td>
+    <td>Student 5 index</td>
+    </tr>
+    <tr>
+    <td colspan=2>Student 6 ime i prezime</td>
+    <td>Student 6 index</td>
+    </tr>
+    </table>
+
+
+
+
+</body>
+</html>
